be930275d903dc4b218e858d44ae64b88b03e273
Johnny Hu <johnny.hu@estiam.com>
2023-06-29 15:08:01 +0200

    Update search.go
    Added http server to search

--- cmd/search/search.go
+++ cmd/search/search.go
@@ -4,29 +4,40 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 )
 
 func main() {
-	serverAddr := "localhost:8080"
+	// En tant que client
+	go func() {
+		serverAddr := "localhost:8080"
 
-	conn, err := net.Dial("tcp", serverAddr)
-	if err != nil {
-		log.Fatal("Error connecting to server:", err)
-	}
-	defer conn.Close()
+		conn, err := net.Dial("tcp", serverAddr)
+		if err != nil {
+			log.Fatal("Error connecting to server:", err)
+		}
+		defer conn.Close()
 
-	message := "Hello from search!"
-	_, err = conn.Write([]byte(message))
-	if err != nil {
-		log.Fatal("Error sending data:", err)
-	}
+		message := "Hello from search client!"
+		_, err = conn.Write([]byte(message))
+		if err != nil {
+			log.Fatal("Error sending data:", err)
+		}
 
-	buffer := make([]byte, 1024)
-	n, err := conn.Read(buffer)
-	if err != nil {
-		log.Fatal("Error receiving response:", err)
-	}
+		buffer := make([]byte, 1024)
+		n, err := conn.Read(buffer)
+		if err != nil {
+			log.Fatal("Error receiving response:", err)
+		}
 
-	response := buffer[:n]
-	fmt.Println("Server response:", string(response))
+		response := buffer[:n]
+		fmt.Println("Server response:", string(response))
+	}()
+
+	// En tant que serveur HTTP
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello from search server!")
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
