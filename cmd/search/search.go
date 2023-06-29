0eda7fe257ada7bde7ee0bbaeb23a23985e979da
Johnny Hu <johnny.hu@estiam.com>
2023-06-29 17:32:46 +0200

    Update search.go
    added http get test method

--- cmd/search/search.go
+++ cmd/search/search.go
@@ -7,9 +7,26 @@ import (
 	"net/http"
 	"encoding/json"
 	"github.com/Funsaiki/Go-SearchEngine/pkg/protocol"
+//	"github.com/Funsaiki/Go-SearchEngine/pkg/donnees"
+	"time"
 )
 
+type Site struct {
+	ID       int       `json:"id"`
+	Name     string    `json:"name"`
+	URL      string    `json:"url"`
+	PageID   int       `json:"page_id"`
+	LastSeen time.Time `json:"lastseen"`
+}
+
+var sites []Site
+
 func main() {
+	sites = append(sites, Site{ID: 1, Name: "Site 1", URL: "https://site1.com", PageID: 123, LastSeen: time.Now()})
+	sites = append(sites, Site{ID: 2, Name: "Site 2", URL: "https://site2.com", PageID: 456, LastSeen: time.Now()})
+
+	http.HandleFunc("/sites", handleSites)
+	
 	// En tant que client
 	go func() {
 		serverAddr := "localhost:8080"
@@ -64,4 +81,39 @@ func main() {
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
+
+	url := "http://localhost:8081/sites"
+
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	var sites []string
+	err = json.NewDecoder(response.Body).Decode(&sites)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(sites)
+}
+
+func handleSites(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		// Récupérer la liste des sites
+		responseData, err := json.Marshal(sites)
+		if err != nil {
+			http.Error(w, "Error encoding sites response", http.StatusInternalServerError)
+			return
+		}
+
+		// Définir l'en-tête Content-Type sur application/json
+		w.Header().Set("Content-Type", "application/json")
+
+		// Envoyer la réponse
+		w.Write(responseData)
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
 }
